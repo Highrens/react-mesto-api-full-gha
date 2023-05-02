a3811e370527e246a71ebbeee8b0cc3c74508025
Sergei Pankov <sergeipankov98@mail.ru>
2023-05-02 22:24:52 +0400

    eslint

--- backend/app.js
+++ backend/app.js
@@ -65,6 +65,7 @@ app.use((req, res, next) => {
 
 app.use(errorLogger);
 app.use(errors());
+// eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
